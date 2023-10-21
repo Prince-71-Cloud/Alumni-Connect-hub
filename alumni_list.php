219543f617aea9fe1802c2ab8554c0164f0ac393
Aman Bhuiyan <82651729+Prince-71-Cloud@users.noreply.github.com>
2023-10-21 21:55:58 +0600

    Add files via upload

---
+++ alumni_list.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Find Alumni</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/fontawesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/alumni_search.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div class="search_container">
+        <form method="post"  action="" class="search-bar mt-4">
+            <input type="text" class="alumni_search" placeholder="Search Alumni">
+            <button type="submit"> <img src="img/search.png"> </button>
+        </form>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="app, landing, corporate, Creative, Html Template, Template">
+    <meta name="author" content="web-themes">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- title -->
+    <title>Alumni of CSE</title>
+
+    <!-- favicon -->
+    <!-- <link href="img/favicon.png" type="image/png" rel="icon"> -->
+
+    <!-- all css here -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/fontawesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/owl.carousel.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <section class="header">
+        <a href="#" class="logo"><img src="img/connection.png" alt="">Alumni</a>
+        <nav class="navbar">
+            <a href="#home">Home</a>
+            <a href="#faculty">Faculty Member</a>
+            <a href="#alumni">Alumni</a>
+            <a href="#running-student">Running Student</a>
+            <a href="#event">Events</a>
+            <a href="#fund">Fund</a>
+            <a href="#jobs">Jobs</a>
+            <a href="https://signup.php">Sign Up</a>
+        </nav>
+        <div id="menu-btn" class="fas fa-bars">
+            <p></p>
+        </div>
+        </section>
+    
+    <!-- card area start -->
+    <div class="card_wrapper" id="faculty">
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h2 class="head_text">Our Faculty Members</h2>
+                    <p class="head_para">Aenean at ligula massa. Donec ipsum elit, placenta sed duierrut<br> dapibus semper turpin Fusce nec premium nuns.</p>
+                </div>
+                <div class="col-12">
+                    <div class="owl-carousel slider_carousel">
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-01.png" alt="">
+                            <div class="card_text">
+                                <h4>Name </h4>
+                                <p>Software Engineer. Orbitax</p>
+                            </div>
+                        </div>
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-03.png" alt="">
+                            <div class="card_text">
+                                <h4>Name</h4>
+                                <p>Network Engineer. California</p>
+                            </div>
+                        </div>
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-04.png" alt="">
+                            <div class="card_text">
+                                <h4>Name</h4>
+                                <p>Software Engineer. TigerIT</p>
+                            </div>
+                        </div>
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-05.png" alt="">
+                            <div class="card_text">
+                                <h4>Name</h4>
+                                <p>Security Engineer, Banglalink</p>
+                            </div>
+                        </div>
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-01.png" alt="">
+                            <div class="card_text">
+                                <h4>Name</h4>
+                                <p>Head of the Marketing, Banglalink</p>
+                            </div>
+                        </div>
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-03.png" alt="">
+                            <div class="card_text">
+                                <h4>Profile ID: 23456</h4>
+                                <p>1901 Thornridge Cir. Shiloh, Hawaii 81063</p>
+                            </div>
+                        </div>
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-04.png" alt="">
+                            <div class="card_text">
+                                <h4>Profile ID: 23456</h4>
+                                <p>1901 Thornridge Cir. Shiloh, Hawaii 81063</p>
+                            </div>
+                        </div>
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-05.png" alt="">
+                            <div class="card_text">
+                                <h4>Profile ID: 23456</h4>
+                                <p>1901 Thornridge Cir. Shiloh, Hawaii 81063</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="show_more">
+                <a href="fsearch.html" class="button beautiful-button">See More Faculty</a>
+            </div>
+        </div>
+    </div>
+    <!-- card area end -->
+
+    <!-- Alumni area start -->
+    <div class="card_wrapper card_wrapper2" id="alumni">
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h2 class="head_text">Our Faculty Alumni</h2>
+                    <p class="head_para">Aenean at ligula massa. Donec ipsum elit, placenta sed duierrut<br> dapibus semper turpin Fusce nec premium nuns.</p>
+                </div>
+                <div class="col-12">
+                    <div class="owl-carousel slider_carousel">
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-01.png" alt="">
+                            <div class="card_text">
+                                <h4>Name </h4>
+                                <p>Software Engineer. Orbitax</p>
+                            </div>
+                        </div>
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-03.png" alt="">
+                            <div class="card_text">
+                                <h4>Name</h4>
+                                <p>Network Engineer. California</p>
+                            </div>
+                        </div>
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-04.png" alt="">
+                            <div class="card_text">
+                                <h4>Name</h4>
+                                <p>Software Engineer. TigerIT</p>
+                            </div>
+                        </div>
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-05.png" alt="">
+                            <div class="card_text">
+                                <h4>Name</h4>
+                                <p>Security Engineer, Banglalink</p>
+                            </div>
+                        </div>
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-01.png" alt="">
+                            <div class="card_text">
+                                <h4>Name</h4>
+                                <p>Head of the Marketing, Banglalink</p>
+                            </div>
+                        </div>
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-03.png" alt="">
+                            <div class="card_text">
+                                <h4>Profile ID: 23456</h4>
+                                <p>1901 Thornridge Cir. Shiloh, Hawaii 81063</p>
+                            </div>
+                        </div>
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-04.png" alt="">
+                            <div class="card_text">
+                                <h4>Profile ID: 23456</h4>
+                                <p>1901 Thornridge Cir. Shiloh, Hawaii 81063</p>
+                            </div>
+                        </div>
+                        <div class="card_box">
+                            <img class="img-fluid w-100" src="img/slider-05.png" alt="">
+                            <div class="card_text">
+                                <h4>Profile ID: 23456</h4>
+                                <p>1901 Thornridge Cir. Shiloh, Hawaii 81063</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="show_more">
+                <a href="http://localhost/alumni/alumni_list.php" class="button beautiful-button2">See More Alumni</a>
+            </div>
+        </div>
+    </div>
+    <!-- Alumni area end -->
+    <!-- footer section start -->
+    <footer style="display: grid;">
+        <p class="developer">Developed By: Aman Bhuiyan</p>
+        <p class="mail"><a href="mailto:bhuiyanaman71@gmail.com">bhuiyanaman71@gmail.com</a></p>
+      </footer>
+    <!-- footer section end -->
+    <!-- all js here -->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+    <script>
+        
+        function slider_carouselInit() {
+            $('.owl-carousel.slider_carousel').owlCarousel({
+                dots: false,
+                loop: true,
+                margin: 30,
+                stagePadding: 2,
+                autoplay: false,
+                nav: true,
+                navText: ["<i class='far fa-arrow-alt-circle-left'></i>","<i class='far fa-arrow-alt-circle-right'></i>"],
+                autoplayTimeout: 1500,
+                autoplayHoverPause: true,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    768: {
+                        items: 2,
+                    },
+                    992: {
+                        items: 5
+                    }
+                }
+            });
+        }
+        slider_carouselInit();
+
+    </script>
+</body>
+</html>
